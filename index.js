7c3beae6575598670c3dde299caaae840d7ecf2f
unknown <igorlopes41@hotmail.com>
2022-10-06 23:44:30 -0300

    Calculos

--- index.js
+++ index.js
@@ -1,73 +1,79 @@
 var conta = document.querySelector("#conta");
-var pessoas = document.querySelector("#pessoas")
-var botao = document.getElementById("teste");
-var erroLabel = document.querySelector("#erroLabel");
-const wrapper = document.getElementById("porcentos");
+var pessoas = document.querySelector("#pessoas");
+var botao = document.querySelector(".btn-prc");
+var erroLabelConta = document.querySelector("#erroLabelConta");
+var erroLabelPessoa = document.querySelector("#erroLabelPessoa");
+const porcentos = document.getElementById("porcentos");
+const calcula =document.querySelector(".btn-calc");
+const valorGorjetaPorPessoa = document.querySelector("#valorGorjeta");
+const valorTotalPorPessoa = document.querySelector("#valorTotal");
 let prevButton = null;
+let checkpoint = 0;
+let valorGorjeta;
+let valorTotal;
 
-wrapper.addEventListener("click", (e) => {
-  const isButton = e.target.nodeName === "BUTTON";
 
-  if (!isButton) {
+porcentos.addEventListener("click", (e) => {
+  const eUmButton = e.target.nodeName === "BUTTON";
+
+  if (!eUmButton) {
     return;
   }
 
-  e.target.classList.add("active"); // Add .active CSS Class
-  setErro(conta);
-  setErro(pessoas);
+  e.target.classList.add("active");
+  checkpoint = 1;
   if (prevButton !== null) {
-    prevButton.classList.remove("active"); // Remove .active CSS Class
-    removeErro(conta);
-    removeErro(pessoas);
+    prevButton.classList.remove("active");
   }
-
   prevButton = e.target;
 });
 
-/* inputs.addEventListener('input' , function () {
-  if(inputs.value.length < 1){
-    inputs.style.outline = "2px solid red ";
-  }else{
-   
-  }
-}) */
 
 
-function setErro (x){
-   
-  if(x.value.length < 1){
-    x.style.outline = "2px solid var(--redErro)";
-    erroLabel.style.display = "block";
-  }
-}
-function removeErro (x){
-  x.style.outline = "none";
-  erroLabel.style.display = "none";
-  
-}
-
-/* function removeErro (y){
-  if(y.value.length >= 1){
-    y.style.outline = "none";
-    erroLabel.style.display = "none";
-  }
-} */
-
-conta.addEventListener('input',function () {
-  if(conta.value.length >= 1){
-    conta.style.outline = "none";
-    erroLabel.style.display = "none";
-  }else {
+function validaConta() {
+  if(conta.value <= 0){
     conta.style.outline = "2px solid var(--redErro)";
-    erroLabel.style.display = "block";
+    erroLabelConta.style.display="block";
+  }else{
+    conta.style.outline = "2px solid var(--strong_cyan)";
+    erroLabelConta.style.display="none";
+    checkpoint++;
   }
-}); 
+}
 
-/* pessoas.addEventListener('input', setErro(pessoas));  */
+function validaPessoas() {
+  if(pessoas.value <= 0){
+    pessoas.style.outline = "2px solid var(--redErro)";
+    erroLabelPessoa.style.display="block";
+  }else{
+    pessoas.style.outline = "2px solid var(--strong_cyan)";
+    erroLabelPessoa.style.display="none";
+    checkpoint++;
+  }
+}
 
-/* botao.addEventListener('click', function(){
+function calculoGorjeta (x,y,z){
+
+  valorGorjeta = (((x.value * y.value)/100)/z.value).toFixed(2);
+  valorGorjetaPorPessoa.innerHTML=valorGorjeta;
+}
+
+function calculoTotal (){
   
-  conta.addEventListener('input', setErro(conta));
-  pessoas.addEventListener('input', setErro(pessoas));
-  console.log(botao.value);
-}) */
+  valorGorjeta = Number(valorGorjeta);
+  valorTotal = ((conta.value / pessoas.value) + (valorGorjeta)).toFixed(2);
+  valorTotalPorPessoa.innerHTML=valorTotal;
+}
+
+calcula.addEventListener('click', function(){
+
+  validaConta();
+  validaPessoas();
+
+  if(prevButton == null || checkpoint < 3){
+    alert("Informe todos os dados");
+  }else{
+    calculoGorjeta(conta, prevButton, pessoas);
+    calculoTotal();
+  }
+})
